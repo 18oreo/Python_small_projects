1b8d5a0443b378dcdeefb17a729a628947734369
Arindam Das <code.arindam189@gmail.com>
2026-08-06 17:39:15 +0530

    terminal
    Added installation instruction for pypdf package.

--- pdf_with_password.py
+++ pdf_with_password.py
@@ -6,3 +6,4 @@ writer.append(reader)
 writer.encrypt("") # enter a password you want
 
 with open("protected.pdf", "wb") as file : writer.write(file)
+  # pip install pypdf  install this in terminal
