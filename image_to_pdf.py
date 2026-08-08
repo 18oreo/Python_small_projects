834e8c2e6eda5fb76b6aad510677b7017697d2b9
Arindam Das <code.arindam189@gmail.com>
2026-08-08 23:56:35 +0530

    pillow
    Added a comment to install Pillow as a required dependency.

--- image_to_pdf.py
+++ image_to_pdf.py
@@ -5,3 +5,4 @@ if file:
     pdf = file.rsplit('.', 1)[0] + '.pdf'
     Image.open(file).convert('RGB').save(pdf)
     print(f"Successfully saved as: {pdf}")
+# pip install Pillow (required dependency)
