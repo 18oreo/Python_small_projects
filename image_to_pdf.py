fe59913cadc610c5d8afc0fc13ebd0021231c1c4
Arindam Das <code.arindam189@gmail.com>
2026-08-08 23:55:27 +0530

    Add files via upload

---
+++ image_to_pdf.py
@@ -0,0 +1,7 @@
+from PIL import Image
+file = "" #Paste your image file path right here, INSIDE the quote
+
+if file:
+    pdf = file.rsplit('.', 1)[0] + '.pdf'
+    Image.open(file).convert('RGB').save(pdf)
+    print(f"Successfully saved as: {pdf}")
